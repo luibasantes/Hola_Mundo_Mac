eb33a4d8cafac2b58a6f54a924e220d11111bce5
luibasantes <luibasantes@hotmail.com>
2018-07-03 16:37:13 -0500

    Subiendo el hola mundo

---
+++ Hola_Mundo_Mac/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hola_Mundo_Mac
+//
+//  Created by Luigi Basantes on 7/3/18.
+//  Copyright © 2018 Luigi Basantes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
